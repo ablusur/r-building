a079d9f4f1bb46869ea3b5a66a136f25909f057d
Ablusur <mdablusur@gmail.com>
2024-05-15 15:03:40 +0600

    footer sec add

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -4,28 +4,47 @@ import { Container, Row, Col } from 'react-bootstrap'
 import Pointer from "../../assets/pointer1.png"
 
 
+
 const Works = () => {
   return (
-    <Container>
-      <Row>
-        <Col lg={4}>
+    <Container-fluid>
+      <Row className='work_main'>
+        <Col lg={3}>
           <div className="work_test">
             <h2>How we work</h2>
             <h6>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, debitis.</h6>
             <a href="#">Get in touch with us</a>
           </div>
         </Col>
-        <Col lg={8}>
+        <Col lg={3}>
           <div className="about_item">
             <div className="a_icon">
               <img src={Pointer} alt="pointer" />
             </div>
             <h3>Strategy</h3>
-            <p>Lorem ipsum dolor sit.</p>
+            <h6>Lorem ipsum dolor sit.</h6>
+          </div>
+        </Col>
+        <Col lg={3}>
+          <div className="about_item">
+            <div className="a_icon">
+              <img src={Pointer} alt="pointer" />
+            </div>
+            <h3>Strategy</h3>
+            <h6>Lorem ipsum dolor sit.</h6>
+          </div>
+        </Col>
+        <Col lg={3}>
+          <div className="about_item">
+            <div className="a_icon">
+              <img src={Pointer} alt="pointer" />
+            </div>
+            <h3>Strategy</h3>
+            <h6>Lorem ipsum dolor sit.</h6>
           </div>
         </Col>
       </Row>
-    </Container>
+    </Container-fluid>
   )
 }
 
