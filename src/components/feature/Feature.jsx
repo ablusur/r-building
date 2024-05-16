95a194e475e33ec188b55b4888f0c79f664f424c
Ablusur <mdablusur@gmail.com>
2024-05-16 12:48:07 +0600

    feature dinamic

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import { FaUsers, FaStopwatch } from "react-icons/fa";
-// import { FaStopwatch } from "react-icons/fa";
+import { FaUsers , FaStopwatch } from "react-icons/fa";
 import "./feature.css"
 import { TiTick } from "react-icons/ti";
 import { IoAirplaneOutline } from "react-icons/io5";
@@ -9,77 +8,62 @@ import { MdContactSupport } from "react-icons/md";
 import { FaBook } from "react-icons/fa6";
 
 const Feature = () => {
+
+    let jalononame = [
+    {
+        title: "User Clint First",
+        para:"Lorem ipsum dolor adipisicing elit. Dolorum tempore labor delectus in tenetur quam porro1",
+        icon: <FaUsers className="icon_color"/>
+    },
+
+    {
+        title: "Two free revision round",
+        para:"Lorem ipsum dolor adipisicing elit. Dolorum tempore labor delectus in tenetur quam porro2",
+        icon: <TiTick className="icon_color"/>
+    },
+
+    {
+        title: "24/7 Support",
+        para:"Lorem ipsum dolor adipisicing elit. Dolorum tempore labor delectus in tenetur quam porro3",
+        icon: <IoAirplaneOutline className="icon_color"/>
+    },
+
+    {
+        title: "User Clint First",
+        para:"Lorem ipsum dolor adipisicing elit. Dolorum tempore labor delectus in tenetur quam porro4",
+        icon: <MdContactSupport className="icon_color"/>
+    },
+
+    {
+        title: "Quick Delivery",
+        para:"Lorem ipsum dolor adipisicing elit. Dolorum tempore labor delectus in tenetur quam porro5",
+        icon: <FaStopwatch className="icon_color"/>
+    },
+
+    {
+        title: "Hands and support",
+        para:"Lorem ipsum dolor adipisicing elit. Dolorum tempore labor delectus in tenetur quam-6",
+        icon: <FaBook className="icon_color"/>
+    },
+
+
+
+]
   return (
 
     <section id="feature">
         <h2>Lorem ipsum dolor sit amet consectetur.</h2>
     <Container>
         <Row>
+            {jalononame.map((item)=>(
             <Col lg={4}>
                 <div className="feacher_item">
-                    <div className="feature_icon">
-                        <FaUsers className='icon_color'/>
-                    </div>
-                    <h4>User Clint First</h4>
-                    <p>Lorem ipsum dolor adipisicing elit. Dolorum tempore labore, 
-                        delectus in tenetur quam porro?</p>
+                    <div className="feature_icon">{item.icon}</div>
+                    <h4>{item.title}</h4>
+                    <p>{item.para}</p>
                 </div>
             </Col>
-
-            <Col lg={4}>
-                <div className="feacher_item">
-                    <div className="feature_icon">
-                        <TiTick className='icon_color'/>
-                    </div>
-                    <h4>Two free revision round</h4>
-                    <p>Lorem ipsum dolor adipisicing elit. Dolorum tempore labore, 
-                        delectus in tenetur quam porro?</p>
-                </div>
-            </Col>
-
-            <Col lg={4}>
-                <div className="feacher_item">
-                    <div className="feature_icon">
-                        <IoAirplaneOutline className='icon_color'/>
-                    </div>
-                    <h4>Templace custimazitation</h4>
-                    <p>Lorem ipsum dolor adipisicing elit. Dolorum tempore labore, 
-                        delectus in tenetur quam porro?</p>
-                </div>
-            </Col>
-
-            <Col lg={4}>
-                <div className="feacher_item">
-                    <div className="feature_icon">
-                        <MdContactSupport className='icon_color'/>
-                    </div>
-                    <h4>24/7 Support</h4>
-                    <p>Lorem ipsum dolor adipisicing elit. Dolorum tempore labore, 
-                        delectus in tenetur quam porro?</p>
-                </div>
-            </Col>
-
-            <Col lg={4}>
-                <div className="feacher_item">
-                    <div className="feature_icon">
-                        <FaStopwatch className='icon_color'/>
-                    </div>
-                    <h4>Quick Delivery</h4>
-                    <p>Lorem ipsum dolor adipisicing elit. Dolorum tempore labore, 
-                        delectus in tenetur quam porro?</p>
-                </div>
-            </Col>
-
-            <Col lg={4}>
-                <div className="feacher_item">
-                    <div className="feature_icon">
-                        <FaBook className='icon_color'/>
-                    </div>
-                    <h4>Hands and support</h4>
-                    <p>Lorem ipsum dolor adipisicing elit. Dolorum tempore labore, 
-                        delectus in tenetur quam porro?</p>
-                </div>
-            </Col>
+            ))}
         </Row>
     </Container>
     </section>
